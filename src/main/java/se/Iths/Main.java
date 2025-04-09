71c68a0a3abf389da7bd617880753254e91ffab4
MD90-Skola <micheldahl90@outlook.com>
2025-04-09 19:30:57 +0200

    lektion4

--- src/main/java/se/Iths/Main.java
+++ src/main/java/se/Iths/Main.java
@@ -1,46 +1,70 @@
 package se.Iths;
-
 import java.util.Scanner;
 
-// sc = scanner
-// input = det användaren skriver in
-// nextLine();
-// nextInt();
 
-        /*
-        String = text
-        int = heltal
-        double = decimaltal
-        boolean true/false
-         */
+/*                  ( TABELL )
+   nr1==nr2     Om nr1 har samma värde som nr2
+   nr1!=nr2     Om nr1 inte har samma värde som nr2
+   nr1<nr2      Om nr1 är mindre än nr2
+   nr1>nr2      Om nr1 är större än nr2
+   nr1<=nr2     Om nr1 är mindre än nr2 eller lika med nr2
+   nr1=>nr2     Om nr1 är större än nr2 eller lika med nr2
+ */
+
+
+
+
+
+
+
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in); //  ifall bugg finns
 
-        System.out.println("skriv in ditt namn ");
-        String name = sc.nextLine();
+        String name = "Michel";
         int pinCode = 1337;   // camelCase
+        int input; // här deklarerar vi vår input variabl
 
         System.out.println("Hej! " + name + " Ange PIN");
-        int input = sc.nextInt();    // kommer spara det i input
+        if(sc.hasNextInt())
+        { // då kollar jag om användaren har skrivigt in en int
+
+             input = sc.nextInt();    // kommer spara det i input  // initierar vår input med ett värde
+        }else{
+            input = 0;
+            System.out.println("Endast siffror");
+        }
+
+
+
+
+        if (input == pinCode) {  // om detta inte stämmer!
+            System.out.println("rätt pinkod");
+        } else {
+            System.out.println("fel kod");
+        }
 
         System.out.println(input);
 
-        String test = sc2.nextLine();
-        System.out.println(test);
 
-        // System.out.println("hej " +name);
-/*
+        System.out.println("-----");
+        String color = "red";
+        String color2 = "blue";
 
-    int nr1 = 5;
-        int nr2 = 20;
-        if (nr1==nr2)
+        boolean isEqualColors = color == color2;
+        System.out.println("isEqualColors = " + isEqualColors);   // Arraylist
+        System.out.println("false pga olika färger");
+        System.out.println("------");
+
+        if(color.equals(color2)){
+           // denna koden körs om den har samma värde som color2
+        }else{
+            //då körs denna koden
+        }
 
 
- */
 
 
     }
