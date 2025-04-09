bbec3e490b384562dd2b69491022e6022d4e836d
MD90-Skola <micheldahl90@outlook.com>
2025-04-09 19:40:44 +0200

    lektion4- färdig

--- src/main/java/se/Iths/Main.java
+++ src/main/java/se/Iths/Main.java
@@ -1,71 +1,30 @@
 package se.Iths;
-import java.util.Scanner;
-
-
-/*                  ( TABELL )
-   nr1==nr2     Om nr1 har samma värde som nr2
-   nr1!=nr2     Om nr1 inte har samma värde som nr2
-   nr1<nr2      Om nr1 är mindre än nr2
-   nr1>nr2      Om nr1 är större än nr2
-   nr1<=nr2     Om nr1 är mindre än nr2 eller lika med nr2
-   nr1=>nr2     Om nr1 är större än nr2 eller lika med nr2
- */
-
-
-
-
-
-
-
 
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String name = "Michel";
-        int pinCode = 1337;   // camelCase
-        int input; // här deklarerar vi vår input variabl
 
-        System.out.println("Hej! " + name + " Ange PIN");
-        if(sc.hasNextInt())
-        { // då kollar jag om användaren har skrivigt in en int
+        boolean isRaining = true;
+        boolean isSunny = false;
 
-             input = sc.nextInt();    // kommer spara det i input  // initierar vår input med ett värde
+        /*
+        if(isRaining == true){
+            System.out.println("ta med paraply");
+            // körs om det stämmer
+        }else if(isSunny==true){
+            System.out.println("ta på dig jacka");
         }else{
-            input = 0;
-            System.out.println("Endast siffror");
+            System.out.println("ta med solbrillor");
         }
+*/
 
-
-
-
-        if (input == pinCode) {  // om detta inte stämmer!
-            System.out.println("rätt pinkod");
-        } else {
-            System.out.println("fel kod");
+        if (isRaining == true) {
+            System.out.println("isRaining är true");
         }
-
-        System.out.println(input);
-
-
-        System.out.println("-----");
-        String color = "red";
-        String color2 = "blue";
-
-        boolean isEqualColors = color == color2;
-        System.out.println("isEqualColors = " + isEqualColors);   // Arraylist
-        System.out.println("false pga olika färger");
-        System.out.println("------");
-
-        if(color.equals(color2)){
-           // denna koden körs om den har samma värde som color2
-        }else{
-            //då körs denna koden
-        }
-
-
-
+if(isSunny==true){
+    System.out.println("isSunny är true");
+}
 
     }
 
